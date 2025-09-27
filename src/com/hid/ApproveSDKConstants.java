8cc6bb4a5d38184e113e666b3a912cd351c0a86d
KarthigaBalan28 <karthigabalan99@gmail.com>
2025-09-27 13:23:55 +0530

    Update ApproveSDKConstants.java

--- src/com/hid/ApproveSDKConstants.java
+++ src/com/hid/ApproveSDKConstants.java
@@ -17,16 +17,9 @@ public class ApproveSDKConstants {
 	public static final String HID_PWD_PROMPT_PROGRESS_EVENT_CODE = "5000";
 	public static final String HID_PWD_PROMPT_ERROR_EVENT_CODE = "5001";
 	public static final String HID_PWD_EXPIRED_PROMPT_EVENT_CODE = "5002";
-	public static final String HID_PWD_EXPIRED_EXCEPTION = "PasswordExpiredException";
-	public static final String HID_AUTHENTICATION_EXCEPTION = "AuthenticationException";
 	public static final String HID_TRANSACTION_EXPIRED_CODE = "1000";
-	public static final String HID_TRANSACTION_EXPIRED_EXCEPTION = "TransactionExpiredException";
 	public static final String HID_BIOMETRIC_ERROR = "BiometricError";
 	public static final String HID_BIOMETRIC_FAILED = "BiometricFailed";
-	public static final String HID_UNSUPPORTED_DEVICE_EXCEPTION = "UnsupportedDeviceException";
-    public static final String HID_LOST_CREDENTIALS_EXCEPTION = "LostCredentialsException";
-    public static final String HID_INTERNAL_EXCEPTION = "InternalException";
-    public static final String HID_INVALID_PARAMETER_EXCEPTION = "InvalidParameterException";
     public static final String HID_UNSUPPORTED_DEVICE_CODE = "200";
     public static final String HID_LOST_CREDENTIALS_CODE = "106";
     public static final String HID_INTERNAL_EXCEPTION_CODE = "0";
@@ -36,7 +29,6 @@ public class ApproveSDKConstants {
 	public static final String HID_GENERIC_ERROR_CODE = "6000";
 	public static final String HID_NO_EXCEPTION_CODE = "2000";
 	public static final String HID_AUTH_EXCEPTION_CODE = "5001";
-	public static final String HID_FINGERPRINT_EXCEPTION = "FingerprintException";
 	public static final String HID_BIO_AUTH_NOT_ENABLED = "NoBioAuth";
 	public static final String HID_SUCCESS_MESSAGE = "success";
 	public static final String HID_NO_EXCEPTION = "No Exception";
@@ -55,6 +47,25 @@ public class ApproveSDKConstants {
 	public static final String HID_CODE_SIGN = "sign";
 	public static final CancelationReason HID_CANCELATION_REASON_CANCEL = CancelationReason.USER_CANCEL;
 	public static final CancelationReason HID_CANCELATION_REASON_SUSPICIOUS = CancelationReason.NOTIFY_SUSPICIOUS;
+	
+	//HID Exceptions
+	public static final String HID_FINGERPRINT_EXCEPTION = "FingerprintException";
+	public static final String HID_UNSUPPORTED_DEVICE_EXCEPTION = "UnsupportedDeviceException";
+    public static final String HID_LOST_CREDENTIALS_EXCEPTION = "LostCredentialsException";
+    public static final String HID_INTERNAL_EXCEPTION = "InternalException";
+    public static final String HID_INVALID_PARAMETER_EXCEPTION = "InvalidParameterException";
+	public static final String HID_TRANSACTION_EXPIRED_EXCEPTION = "TransactionExpiredException";
+	public static final String HID_PWD_EXPIRED_EXCEPTION = "PasswordExpiredException";
+	public static final String HID_AUTHENTICATION_EXCEPTION = "AuthenticationException";
+	public static final String HID_FINGERPRINT_AUTHENTICATION_REQUIRED_EXCEPTION = "FingerprintAuthenticationRequiredException";
+	public static final String HID_REMOTE_EXCEPTION = "RemoteException";
+	public static final String HID_SERVER_OPERATION_FAILED_EXCEPTION = "ServerOperationFailedException";
+	public static final String HID_SERVER_UNSUPPORTED_OPERATION_EXCEPTION = "ServerUnsupportedOperationException";
+	public static final String HID_FINGERPRINT_NOT_ENROLLED_EXCEPTION = "FingerprintNotEnrolledException";
+	public static final String HID_PASSWORD_EXPIRED_EXCEPTION = "PasswordExpiredException";
+	public static final String HID_PASSWORD_REQUIRED_EXCEPTION = "PasswordRequiredException";
+	public static final String HID_INAVLID_CONTAINER_EXCEPTION = "InvalidContainerException";
+	public static final String HID_INEXPLICIT_CONTAINER_EXCEPTION = "InexplicitContainerException";
 	public static final String HID_TRANSACTION_CANCELED_EXCEPTION = "TransactionCanceledException";
 
 }
